371928f8c708605078c12b3fb84c40f2040bdc5b
Angel <h2308416011@hcu.edu.my>
2025-07-01 00:59:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function creditOnClick() {
+  alert("Hello! \nThis is made by Angeline Ong Xing Yen, H2308416011. \nFor Q&A or Feedbacks visit: @AngelDev on Instagram");
+}
+// can be deleted. Just a small effect for test, will not effect html if deleted.
